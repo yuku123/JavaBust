dee40e119111ec498d21424c280a758eeed11dda
子房 <chanhao.zhang@come-future.com>
2021-11-01 22:37:03 +0800

    add

---
+++ src/main/java/com/zifang/ex/bust/charpter2/CharEncoding.java
@@ -0,0 +1,9 @@
+package com.zifang.ex.bust.charpter2;
+
+public class CharEncoding {
+    public static void main(String[] args) {
+        char c0 = '\u5f20';
+        System.out.println();
+
+    }
+}
