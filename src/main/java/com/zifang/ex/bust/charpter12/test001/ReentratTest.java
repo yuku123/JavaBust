42f8bbb5acd506fa6954e9624c14107d3339ca87
子房 <chanhao.zhang@come-future.com>
2021-12-22 13:59:00 +0800

    add

--- src/main/java/com/zifang/ex/bust/charpter12/test001/ReentratTest.java
+++ src/main/java/com/zifang/ex/bust/charpter12/test001/ReentratTest.java
@@ -2,6 +2,7 @@ package com.zifang.ex.bust.charpter12.test001;
 
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -71,6 +72,67 @@ public class ReentratTest {
             }finally {
                 lock_test003.unlock();
             }
+        }
+    }
+
+    private static final Lock lock_test004_1 = new ReentrantLock();
+    private static final Lock lock_test004_2 = new ReentrantLock();
+
+    @Test
+    public void test004(){
+        Thread thread1 = new Thread(()-> test004_innerFunction(lock_test004_1,lock_test004_2),"线程1");
+        Thread thread2 = new Thread(()-> test004_innerFunction(lock_test004_2,lock_test004_1),"线程2");
+        thread1.start();
+        thread2.start();
+        thread1.interrupt();
+
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test004_innerFunction(Lock lock1,Lock lock2){
+        try {
+            lock1.lockInterruptibly();
+            Thread.sleep(1000);
+            lock2.lockInterruptibly();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock1.unlock();
+            lock2.unlock();
+            System.out.println(Thread.currentThread().getName() + "正常执行");
+        }
+    }
+
+
+    private static final Lock lock_test005 = new ReentrantLock();
+    @Test
+    public void test005(){
+        Thread thread1 = new Thread(()-> test005_innerFunction(lock_test005),"线程1");
+        Thread thread2 = new Thread(()-> test005_innerFunction(lock_test005),"线程2");
+        thread1.start();
+        thread2.start();
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test005_innerFunction(Lock lock){
+        try {
+            if (lock.tryLock(1, TimeUnit.SECONDS)) {
+                System.out.println("等待前，"+Thread.currentThread().getName());
+                Thread.sleep(3000);
+                System.out.println("等待后,"+Thread.currentThread().getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 }
